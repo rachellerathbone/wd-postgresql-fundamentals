0e986d8bd41b9bd238c63ace87de0cf21b614aa8
rrathbone <rachellerathbone@gmail.com>
2016-09-28 16:23:56 -0700

    complete exercises 2 and 3

--- lib/part2.sql
+++ lib/part2.sql
@@ -8,36 +8,43 @@ SELECT * FROM restaurants;
 Select the names of all the restaurants.
 */
 
+SELECT name FROM restaurants
 
 /* Exercise selectAllCustomerNames
 Select the names of all the customers.
 */
 
+SELECT name FROM customers
 
 /* Exercise selectCustomerById
 Select all columns of a single customer with an id of 1.
 */
 
+SELECT * FROM customers WHERE id = 1
 
 /* Exercise selectLocationById
 Select all columns of a single location with an id of 3.
 */
 
+SELECT * FROM locations WHERE id = 3
 
 /* Exercise selectCountOfAllLocationsByCity
 Select the count of all the locations by city.
 */
 
+SELECT COUNT(city) FROM locations
 
 /* Exercise selectDistinctCountOfAllLocationsByCity
 Select the count of all the unique cities with locations.
 */
 
+SELECT COUNT(DISTINCT city) FROM locations
 
 /* Exercise selectCheapestDish
 Select all columns the cheapest dish
 */
 
+SELECT * FROM dishes WHERE cost = (SELECT MIN(cost) FROM dishes);
 
 /* Exercise insertRestaurant
 Insert the following row into the restaurants table:
@@ -50,6 +57,9 @@ created_at | 2000-04-22 00:00:00 UTC
 updated_at | 2000-04-22 00:00:00 UTC
 */
 
+INSERT INTO restaurants (name, kind, bio, dollars, opened_at, created_at, updated_at)
+VALUES ('Sheesh Kebap', 'Iranian', 'Be amazed, our chef bobs like no other!','4', '1977-03-01 00:00:00 UTC', '2000-04-22 00:00:00 UTC', '2000-04-22 00:00:00 UTC');
+
 
 /* Exercise insertCustomer
 Insert the following row into the customers table
@@ -58,6 +68,9 @@ email      | loveIt@bomb.com
 created_at | 2000-04-22 00:00:00 UTC
 updated_at | 2000-04-22 00:00:00 UTC
 */
+
+INSERT INTO customers (name, email, created_at, updated_at)
+VALUES ('the connoisseur', 'loveIt@bomb.com', '2000-04-22 00:00:00 UTC', '2000-04-22 00:00:00 UTC');
 
 
 /* Exercise insertDish
@@ -72,6 +85,9 @@ created_at     | 2000-04-22 00:00:00 UTC
 updated_at     | 2000-04-22 00:00:00 UTC
 */
 
+INSERT INTO dishes (restaurant_id, name, description, cost, vegetarian_at, gluten_free_at, created_at, updated_at)
+VALUES ('1', 'The Bog Dog', 'This dawg hails from the depths of the bog! Made with real alligator.', '9.65', null, null, '2000-04-22 00:00:00 UTC', '2000-04-22 00:00:00 UTC');
+
 
 /* Exercise insertReservation
 Insert the following row into the reservations table
@@ -84,32 +100,43 @@ created_at        | 2000-04-22 00:00:00 UTC
 updated_at        | 2000-04-22 00:00:00 UTC
 */
 
+INSERT INTO reservations (customer_id, restaurant_id, wants_vegetarian, wants_gluten_free, confirmed_at, created_at, updated_at)
+VALUES ('4', '3', 'true', 'false', '2000-04-22 00:00:00 UTC', '2000-04-22 00:00:00 UTC', '2000-04-22 00:00:00 UTC');
 
 /* Exercise updateCustomerName
 Update the 'Little baby Tomkins' customer's name to 'Big Tom Tomkins'
 */
+
+UPDATE customers SET name = 'Big Tom Tomkins' WHERE name = 'Little baby Tomkins';
 
 
 /* Exercise updateLocationById
 Update the id = 3 location's street to '555 No Way' and city to 'Olympia'
 */
 
+UPDATE locations SET street = '555 No Way', city = 'Olympia' WHERE id = 3;
 
 /* Exercise updateDishWithChicken
 Update "Melinda's salad blend" to not be a vegetarian
 */
+
+UPDATE dishes SET vegetarian_at = NULL WHERE name = 'Melinda''s salad blend';
 
 
 /* Exercise deleteCustomer
 Delete customers whose name is "Prince"
 */
 
+DELETE FROM customers WHERE name = 'Prince';
 
 /* Exercise deleteRestaurantsByDollar
 Delete restaurants with more than 2 dollar signs.
 */
 
+DELETE FROM restaurants WHERE dollars > 2;
 
 /* Exercise deleteAllNonVegetarianDishes
 Delete all non-vegetarian dishes
 */
+
+DELETE FROM dishes WHERE vegetarian_at is not null;
--- lib/part3.sql
+++ lib/part3.sql
@@ -6,18 +6,25 @@ customer      | the name of the customer
 venue         | the name of the restaurant
 rating        | the rating in the customer review
 review        | the comment from the customer review
+
 */
 
+SELECT customers.name AS customer, restaurants.name AS venue, rating, comment AS review
+FROM reviews
+INNER JOIN restaurants ON restaurants.id = reviews.restaurant_id
+INNER JOIN customers ON customers.id = reviews.customer_id;
 
 /* Exercise getAllReservations
 Get all reservations w/ confirm time, customer name, restaurant name
-
 It would have the following columns:
 customer      | the name of the customer
 venue         | the name of the restaurant
 confirmed_at  | the confirmation time
 */
-
+SELECT customers.name AS customer, restaurants.name AS venue, confirmed_at
+FROM reservations
+INNER JOIN restaurants ON restaurants.id = reservations.restaurant_id
+INNER JOIN customers ON customers.id = reservations.customer_id;
 
 /* Exercise getVegetarianReservations
 Get vegetarian reservations w/ customer & restaurant names
@@ -27,7 +34,11 @@ customer      | the name of the customer
 venue         | the name of the restaurant
 confirmed_at  | the confirmation time
 */
-
+SELECT customers.name AS customer, restaurants.name AS venue, confirmed_at
+FROM reservations
+INNER JOIN restaurants ON restaurants.id = reservations.restaurant_id
+INNER JOIN customers ON customers.id = reservations.customer_id
+WHERE wants_vegetarian = true;
 
 /* Exercise getReviewsByCustomer3
 Get reviews by the customer with id #3
@@ -37,7 +48,10 @@ venue         | the name of the restaurant
 rating        | the rating in the customer review
 review        | the comment from the customer review
 */
-
+SELECT restaurants.name AS venue, rating, comment AS review
+FROM reviews
+INNER JOIN restaurants ON restaurants.id = reviews.restaurant_id
+WHERE customer_id = 3;
 
 /* Exercise getBadReviews
 Get bad reviews (rating below 4) with customer name & restaurant name.
@@ -49,7 +63,12 @@ restaurant    | the name of the restaurant
 rating        | the rating in the customer review
 review        | the comment from the customer review
 */
-
+SELECT customers.name AS customer, restaurants.name AS restaurant, rating, comment AS review
+FROM reviews
+INNER JOIN restaurants ON restaurants.id = reviews.restaurant_id
+INNER JOIN customers ON customers.id = reviews.customer_id
+WHERE rating < '4';
+-- ALTER TABLE reviews ALTER COLUMN rating TYPE integer;
 
 /* Exercise getGlutenFreeDishes
 Get gluten free dishes at restaurants that have min 1 perfect rating
@@ -60,7 +79,11 @@ restaurant    | the name of the restaurant
 cost          | how much the dish costs
 expense_level | the dollar rating of the restaurant
 */
-
+SELECT dishes.name AS dish, restaurants.name AS restaurant, cost, restaurants.dollars AS expense_level
+FROM dishes
+INNER JOIN restaurants ON restaurants.id = dishes.restaurant_id
+INNER JOIN reviews ON restaurants.id = reviews.restaurant_id
+WHERE rating = '5' AND gluten_free_at IS NOT NULL;
 
 /* Exercise getGlutenFreeOrVegetarianDishes
 Get dishes that are gluten free and/or vegetarian at restaurants that have min 1 perfect rating
@@ -71,3 +94,8 @@ restaurant    | the name of the restaurant
 cost          | how much the dish costs
 expense_level | the dollar rating of the restaurant
 */
+SELECT dishes.name AS dish, restaurants.name AS restaurant, cost, restaurants.dollars AS expense_level
+FROM dishes
+INNER JOIN restaurants ON restaurants.id = dishes.restaurant_id
+INNER JOIN reviews ON restaurants.id = reviews.restaurant_id
+WHERE rating = '5' AND (gluten_free_at IS NOT NULL OR vegetarian_at IS NOT NULL);
